65961b0dd4209d427fbb46cf120e579eb4965a07
Angelzaki <angel.salazarmaylle@gmail.com>
2025-05-08 23:25:33 -0500

    fix: ajuste para usar audio en la ia

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -4,15 +4,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.VITE_APIKEY,
-  authDomain: process.env.VITE_AUTH_DOMAIN,
-  projectId: process.env.VITE_PROJECT_ID,
-  storageBucket: process.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_APP_ID,
-  measurementId: process.env.VITE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCP1qGEC32JdAdaQ3pk3xgNq32vYXPqB70",
+  authDomain: "chatderm-a65c0.firebaseapp.com",
+  projectId: "chatderm-a65c0",
+  storageBucket: "chatderm-a65c0.firebasestorage.app",
+  messagingSenderId: "1038720370031",
+  appId: "1:1038720370031:web:c8769db7517ab15da8549a",
+  measurementId: "G-0PKKSCF9BJ"
 };
-
 
 const app = initializeApp(firebaseConfig);
 
